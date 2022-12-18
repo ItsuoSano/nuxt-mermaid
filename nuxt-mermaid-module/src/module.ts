50f310554dd8b720641103d56a4d0b212eaddf89
ItsuoSano <itsuo.sano@yourmystar.jp>
2022-12-18 10:08:35 +0900

    カレンダー様に Vueファイルを置いておく

--- nuxt-mermaid-module/src/module.ts
+++ nuxt-mermaid-module/src/module.ts
@@ -1,24 +1,22 @@
-import { fileURLToPath } from "url";
-import { defineNuxtModule, createResolver, addComponent } from "@nuxt/kit";
+import { fileURLToPath } from 'url'
+import { defineNuxtModule, createResolver, addComponent } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
-    name: "nuxt-mermaid",
-    configKey: "nuxtMermaid",
+    name: 'nuxt-mermaid',
+    configKey: 'nuxtMermaid',
   },
   setup(options, nuxt) {
-    nuxt.hook("nitro:config", (nitroConfig) => {
-      nitroConfig.runtimeConfig = nitroConfig.runtimeConfig || {};
-      nitroConfig.runtimeConfig.content.markdown.remarkPlugins = {
-        "@nuxt-mermaid/remark-mermaid-plugin": {},
-      };
-    });
-    const { resolve } = createResolver(import.meta.url);
-    const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
+    nuxt.hook('nitro:config', (nitroConfig) => {
+      nitroConfig.runtimeConfig = nitroConfig.runtimeConfig || {}
+      nitroConfig.runtimeConfig.content.markdown.remarkPlugins = { '@nuxt-mermaid/remark-mermaid-plugin': {} }
+    })
+    const { resolve } = createResolver(import.meta.url)
+    const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
     addComponent({
-      name: "NuxtMermaid",
-      filePath: resolve(runtimeDir, "NuxtMermaid.ts"),
+      name: 'NuxtMermaid',
+      filePath: resolve(runtimeDir, 'NuxtMermaid.vue'),
       global: true,
-    });
+    })
   },
-});
+})
